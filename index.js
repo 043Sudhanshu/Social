2d318022231f69d4ce16af22f96e2e1ea20958bf
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-09-27 15:18:24 +0530

    added routes and controller folders

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const port=8000;
 
 const app=express();
 
-const path=require('path');
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+// const path=require('path');
+// app.set('view engine','ejs');
+// app.set('views',path.join(__dirname,'views'));
 
-app.use(express.urlencoded());
-app.use(express.static('assets'));
+// app.use(express.urlencoded());
+// app.use(express.static('assets'));
+
+app.use('/',require('./routes/index.js'));
 
 app.listen(port,function(err){
     if(err){console.log(`Error in running server: ${err}`);}
