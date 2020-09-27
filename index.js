6a3778a07c51f494f91855381fec0ea34b40928f
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-09-27 12:48:54 +0530

    added middlewares to parse form data and static files

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express=require('express');
 const port=8000;
 
+
 const app=express();
+
+const path=require('path');
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.use(express.urlencoded());
+app.use(express.static('assets'));
 
 app.listen(port,function(err){
     if(err){console.log(`Error in running server: ${err}`);}
